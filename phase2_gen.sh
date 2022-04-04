24fc9f0fa9146449af6d1911d780224f2c961a16
jtmr05 <62111562+jtmr05@users.noreply.github.com>
2022-04-04 22:31:36 +0100

    adjusted ring size for planets

--- phase2_gen.sh
+++ phase2_gen.sh
@@ -7,6 +7,7 @@ gen(){
         bin/generator box 2 3 resources/box.3d
         bin/generator cone 1 2 4 3 resources/cone.3d
         bin/generator sphere 1 8 8 resources/sphere.3d
+        bin/generator torus 2 4 20 20 resources/ring.3d
         return 0
     else
         echo "error: bin/generator not found" 1>&2
